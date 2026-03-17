15dcb1c0c0350ce342513fce8b2654c1a9c8d69d
emilijawithaj <125661251+emilijawithaj@users.noreply.github.com>
2026-03-17 14:08:03 +0000

    Background Heart Rate collection with passive listener

--- wear/src/main/java/com/example/soverloadtracker/presentation/sensorDataGathering/BackgroundHeartRateService.kt
+++ wear/src/main/java/com/example/soverloadtracker/presentation/sensorDataGathering/BackgroundHeartRateService.kt
@@ -4,15 +4,20 @@ import android.util.Log
 import androidx.health.services.client.PassiveListenerService
 import androidx.health.services.client.data.DataPointContainer
 import androidx.health.services.client.data.DataType
+import com.example.soverloadtracker.SqLiteDatabase
+import com.example.soverloadtracker.presentation.LogData
+import java.time.Instant
 
 class BackgroundHeartRateService() : PassiveListenerService() {
+
     override fun onNewDataPointsReceived(dataPoints: DataPointContainer) {
         val hrPoints = dataPoints.getData(DataType.HEART_RATE_BPM)
         for (point in hrPoints) {
             val bpm = point.value
-            Log.d("HR", "Background BPM: $bpm")
+            Log.d("HRateBG", "Background BPM: $bpm")
 
-            //TODO handle
+            val database = SqLiteDatabase.getInstance(this)
+            database.addLogRecord(LogData(Instant.now(), bpm.toFloat(), 0f, false, 0f, false, false, false, false, false, false, false, false, false, arrayListOf()))
         }
     }
 }
